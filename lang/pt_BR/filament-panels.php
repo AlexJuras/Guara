b17a37ba65db21001c26cfe8e5aaf88c1b900e24
Alex Victor <alexvictorphb@gmail.com>
2025-11-22 13:27:24 -0300

    Filament atualizado para português

---
+++ lang/pt_BR/filament-panels.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+    'label' => 'Navegação',
+    
+    'actions' => [
+        'cancel' => [
+            'label' => 'Cancelar',
+        ],
+        'create' => [
+            'label' => 'Criar',
+            'modal' => [
+                'heading' => 'Criar :label',
+                'submit' => [
+                    'label' => 'Criar',
+                ],
+            ],
+        ],
+        'edit' => [
+            'label' => 'Editar',
+        ],
+        'save' => [
+            'label' => 'Salvar alterações',
+        ],
+        'view' => [
+            'label' => 'Ver',
+        ],
+    ],
+    
+    'breadcrumb' => 'Novo :label',
+    
+    'form' => [
+        'actions' => [
+            'cancel' => [
+                'label' => 'Cancelar',
+            ],
+            'create' => [
+                'label' => 'Criar',
+            ],
+            'create_another' => [
+                'label' => 'Criar e criar outro',
+            ],
+        ],
+    ],
+    
+    'notifications' => [
+        'created' => [
+            'title' => 'Criado com sucesso',
+        ],
+    ],
+    
+    'title' => 'Criar :label',
+];
